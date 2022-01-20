c21ff7bf2db4facd586d406416ba7e65e76ff1d1
امیرحسین عنبری <a.anbari@greenweb.local>
2022-01-20 12:22:14 +0330

    first commit

---
+++ src/RabbitMQServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+namespace AmirAnbari\Rabbitmq;
+
+use Illuminate\Support\ServiceProvider;
+
+class RabbitMQServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config'     => base_path('config'),
+            __DIR__.'/migrations' => base_path('database/migrations'),
+        ]);
+    }
+}
